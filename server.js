eb932704d5b8d4897ad668999f817e8647cba41f
KsushaDk <81408959+KsushaDk@users.noreply.github.com>
2022-05-04 13:09:45 +0300

    add: chalk, dotenv

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 const express = require('express')
+const chalk = require('chalk')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+require('dotenv').config()
 const methodOverride = require('method-override')
 const postRoutes = require('./routes/post-routes')
 const postApiRoutes = require('./routes/api-post-routes')
 const contactRoutes = require('./routes/contact-routes')
 const createPath = require('./helpers/create-path')
 
+const errorMsg = chalk.bgKeyword('white').redBright
+const successMsg = chalk.bgKeyword('green').white
+
 const app = express()
 
 app.set('view engine', 'ejs')
 
-const PORT = 3003
-const db =
-  'mongodb+srv://Kseniya:89988998@cluster0.fxowp.mongodb.net/node-blog?retryWrites=true&w=majority'
-
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((res) => console.log('Connected to DB'))
-  .catch((error) => console.log(error))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log(successMsg('Connected to DB')))
+  .catch((error) => console.log(errorMsg(error)))
 
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`)
+app.listen(process.env.PORT, (error) => {
+  error
+    ? console.log(errorMsg(error))
+    : console.log(successMsg(`listening port ${PORT}`))
 })
 
 app.use(express.urlencoded({ extended: false }))
